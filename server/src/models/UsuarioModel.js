efb92ce480217a23d3b2d19a473c02efda1949bd
ArielGnz <arielgon2206@gmail.com>
2025-03-19 20:01:00 -0300

    usuario model success

---
+++ server/src/models/UsuarioModel.js
@@ -0,0 +1,38 @@
+
+module.exports = (sequelize, DataTypes) => {
+    const Usuario = sequelize.define("Usuario", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primayKey: true,
+        },
+        npmbre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        apellido: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dni: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        cbu: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        cuil: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+
+    });
+
+    Usuario.associate = (models) => {
+        Usuario.hasMany(models.Prestamo, {foreingKey: "usuarioId"});
+    };
+
+    return Usuario;
+}
