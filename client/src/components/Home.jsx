2a48cca8dd15357532ba5e54ad5c9ee22c93ad55
ArielGnz <arielgon2206@gmail.com>
2025-04-11 19:44:30 -0300

    tailwind instalado

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex'>
+        <div className='border-4'>
+            SOCIOS
+        </div>
+
+        <div>
+            AYUDA
+        </div>
+    </div>
+  )
+}
+
+export default Home
