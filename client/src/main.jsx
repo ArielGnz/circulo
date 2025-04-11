c466dc6658f653858c56f1e55e05c9f07f159280
ArielGnz <arielgon2206@gmail.com>
2025-04-11 12:00:59 -0300

    redux instalado

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+//import { StrictMode } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx';
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+//import { createRoot } from 'react-dom/client'
+//import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
+  </Provider>
 )
