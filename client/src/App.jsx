40d82727eee1a95a41dcb9b03ce45c56bec5eaed
ArielGnz <arielgon2206@gmail.com>
2025-04-14 20:32:21 -0300

    arreglando los botones

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="flex justify-center bg-black mx-auto cd w-[100%]"> 
-      <div className="bg-red-300 w-[85%] h-screen">
+      <div className="w-[85%] h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,11 +4,11 @@ const Home = () => {
   return (
     <div className="flex justify-center my-18">
       <div className="border-2 bg-gray-300 mx-10 font-bold rounded-md">
-        <h1 className="text-white px-6 py-4">SOCIOS</h1>
+        <h1 className="text-gray-600 px-10 text-xl py-4">SOCIOS</h1>
       </div>
 
       <div className="border-2 bg-gray-300 mx-10 font-bold rounded-md">
-        <h1 className="text-white px-6 py-4">AYUDA</h1>
+        <h1 className="text-gray-600 px-10 text-xl py-4">AYUDA</h1>
       </div>
     </div>
   );
