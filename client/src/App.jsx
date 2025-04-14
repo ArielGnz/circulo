c9e74f9aca4247929530ee68fb99a47b935a5cca
ArielGnz <arielgon2206@gmail.com>
2025-04-14 20:22:29 -0300

    estilando el home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,14 @@ import Home from "./components/Home";
 
 function App() {
   return (
-    <>
-      <div className="h-screen flex-grow">
+    <div className="flex justify-center bg-black mx-auto cd w-[100%]"> 
+      <div className="bg-red-300 w-[85%] h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </div>
-    </>
+    </div>
   );
 }
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex'>
-        <div className='border-4'>
-            SOCIOS
+    <div className='flex justify-center my-18'>
+        <div className='border-4 bg-gray-300 mx-10 font-semibold'>
+           <h1>SOCIOS</h1>
         </div>
 
-        <div>
+        <div className='border-4 bg-gray-300 mx-10 font-semibold'>
             AYUDA
         </div>
     </div>
