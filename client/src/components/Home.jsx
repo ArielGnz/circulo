3a95d9b404b346ed87e457b9af8852869030dab7
ArielGnz <arielgon2206@gmail.com>
2025-04-14 20:26:32 -0300

    botones de links listos

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='flex justify-center my-18'>
-        <div className='border-4 bg-gray-300 mx-10 font-semibold'>
-           <h1>SOCIOS</h1>
-        </div>
+    <div className="flex justify-center my-18">
+      <div className="border-2 bg-gray-300 mx-10 font-bold rounded-md">
+        <h1 className="text-white px-6 py-4">SOCIOS</h1>
+      </div>
 
-        <div className='border-4 bg-gray-300 mx-10 font-semibold'>
-            AYUDA
-        </div>
+      <div className="border-2 bg-gray-300 mx-10 font-bold rounded-md">
+        <h1 className="text-white px-6 py-4">AYUDA</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
