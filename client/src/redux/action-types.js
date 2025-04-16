aabb91034466211026b512a54e0f37bf57d99980
ArielGnz <arielgon2206@gmail.com>
2025-04-16 20:38:41 -0300

    configurando el redux

---
+++ client/src/redux/action-types.js
@@ -0,0 +1,1 @@
+export const GET_SOCIOS = 'GET_SOCIOS';
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,10 +1,15 @@
-import { 
-    
-} from './action-types';
+import { GET_SOCIOS } from './action-types';
 
 import axios from 'axios';
 
-
 axios.defaults.baseURL = "http://localhost:3001";
 
-
+export const getSocios = () => {
+    return async function(dispatch){
+        const response = await axios('/socios');
+        return dispatch({
+            type: GET_SOCIOS,
+            payload: response.data
+        })
+    }
+}
