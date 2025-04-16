d95cee0d3cdaa1481b9c11cc40ad84b26dd7e1e2
ArielGnz <arielgon2206@gmail.com>
2025-04-16 20:01:53 -0300

    creando rutas en el server

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { allSocios } = require("../handlers/getHandlers");
 
+router.get("/socios", allSocios);
 router.get('/', (req, res) => {
   res.send('API funcionando');
 });
