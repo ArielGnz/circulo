9a46f5baad5c369ef65536dcff81d3e9f3d603a4
ArielGnz <arielgon2206@gmail.com>
2025-04-22 10:54:21 -0300

    configuracion del reducer

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,16 +1,19 @@
 
-import {  } from "./action-types";
+import { GET_SOCIOS } from "./action-types";
 
 const initialState = {
-   
+   socios: [],
 }
 
 const rootReducer = (state = initialState, {type, payload}) => {
 
     switch(type){
-
         
-
+        case GET_SOCIOS:
+            return{
+                ...state,
+                socios: payload,
+            }
     }
 };
 
