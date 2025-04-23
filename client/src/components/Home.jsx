2b79fe18ce1e1c5736ffb2b3fbf10a099b711322
ArielGnz <arielgon2206@gmail.com>
2025-04-23 11:09:39 -0300

    link to ayuda

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,9 +10,11 @@ const Home = () => {
         </div>
       </Link>
 
-      <div className="border-2 bg-gray-300 mx-10 font-bold rounded-md">
-        <h1 className="text-gray-600 px-10 text-xl py-4">AYUDA</h1>
-      </div>
+      <Link to="/ayuda">
+        <div className="border-2 bg-gray-300 mx-10 font-bold rounded-md">
+          <h1 className="text-gray-600 px-10 text-xl py-4">AYUDA</h1>
+        </div>
+      </Link>
     </div>
   );
 };
