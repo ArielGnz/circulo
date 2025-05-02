ede77aa8853572b655314d8458a590a4ca69a34f
ArielGnz <arielgon2206@gmail.com>
2025-05-02 12:16:52 -0300

    actualizando el home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,13 +6,13 @@ const Home = () => {
     <div className="flex justify-center my-18">
       <Link to="/socios">
         <div className="border-2 bg-gray-300 mx-10 font-bold rounded-md">
-          <h1 className="text-gray-600 px-10 text-xl py-4">SOCIOS</h1>
+          <h1 className="text-gray-600 hover:text-white px-10 text-xl py-4">SOCIOS</h1>
         </div>
       </Link>
 
       <Link to="/ayuda">
         <div className="border-2 bg-gray-300 mx-10 font-bold rounded-md">
-          <h1 className="text-gray-600 px-10 text-xl py-4">AYUDA</h1>
+          <h1 className="text-gray-600 hover:text-white px-10 text-xl py-4">AYUDA</h1>
         </div>
       </Link>
     </div>
