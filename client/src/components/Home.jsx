f0a39a70c0d99ce5e1be296b3c40537d7d061fe3
ArielGnz <arielgon2206@gmail.com>
2025-05-05 17:54:42 -0300

    importando imagen del logo

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoNegro from "../assets/logoNegro.jpg"
 
 const Home = () => {
   return (
-    <div className="flex justify-center my-18">
+    <div className="flex flex-col justify-center my-18 border-2 rounded-md border-gray-400">
+      <div>
+
+      </div>
       <Link to="/socios">
         <div className="border-2 mx-10 font-bold rounded-md bg-gray-300 hover:bg-gray-400 text-center transform transition duration-200 hover:scale-105 hover:-translate-y-1">
           <h1 className="text-gray-600 hover:text-white px-10 text-xl py-4">
