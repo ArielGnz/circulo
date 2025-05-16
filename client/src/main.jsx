4c640e7e2eb02ce52468ec3fbb3b608c6ca4b13a
ArielGnz <arielgon2206@gmail.com>
2025-05-16 19:22:01 -0300

    arreglando el front

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
-//import { StrictMode } from 'react'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-//import { createRoot } from 'react-dom/client'
-//import './index.css'
-//import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
