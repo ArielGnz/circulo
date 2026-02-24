7f780ee3aee1f4417ba5a39d835233512eaab0b6
ArielGnz <arielgon2206@gmail.com>
2026-02-24 19:57:35 -0300

    news functions

--- client/src/components/PrestamoList.jsx
+++ client/src/components/PrestamoList.jsx
@@ -45,8 +45,7 @@ const PrestamoList = () => {
   //   const socio = socios.find((s) => s.id === usuarioId);
   //   return socio ? `${socio.apellido}, ${socio.nombre}` : "Desconocido";
   // };
-  //
-  //
+  
 
 
   const obtenerDatosSocio = (prestamo) => {
