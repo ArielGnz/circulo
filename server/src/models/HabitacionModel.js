dc034b5df17449aec94668596b11974c37f3fab9
ArielGnz <arielgon2206@gmail.com>
2026-06-01 20:15:12 -0300

    new model habitacion

---
+++ server/src/models/HabitacionModel.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    const Habitacion = sequelize.define("Habitacion", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        numero: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        estado: {
+            type: DataTypes.ENUM('disponible', 'ocupada', 'mantenimiento'),
+            allowNull: false,
+            defaultValue: 'disponible',
+        },
+    });
+};
+
